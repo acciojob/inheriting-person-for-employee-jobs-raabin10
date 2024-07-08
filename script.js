13084ed3c9e7015b37e62c4b5911040fedcb1c6c
RAABIN SINGH  S A <112077212+raabin10@users.noreply.github.com>
2024-07-08 18:37:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-// complete this js code
-function Person(name, age) {}
+// Person class
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
 
-function Employee(name, age, jobTitle) {}
+Person.prototype.greet = function() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+};
+
+// Employee class that inherits from Person
+function Employee(name, age, jobTitle) {
+    Person.call(this, name, age);
+    this.jobTitle = jobTitle;
+}
+
+// Inherit Person's prototype
+Employee.prototype = Object.create(Person.prototype);
+
+// Set the constructor to Employee
+Employee.prototype.constructor = Employee;
+
+Employee.prototype.jobGreet = function() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+};
 
 // Do not change code below this line
 window.Person = Person;
